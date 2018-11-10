b82c329c709ae301af740c2435945d60675de44e
nagaminenot <nagamine@efg.company>
2018-11-10 20:34:03 +0900

    sqlliteでrails newし、アプリ名をproject_eへ変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_10_092848) do
-
-  create_table "pages", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
