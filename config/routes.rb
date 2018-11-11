a1b0867ffa3c95c3293dbcf3b154d3c75a18be41
nagaminenot <nagamine@efg.company>
2018-11-11 21:40:21 +0900

    routes.rb修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'pages/index'
+  root 'pages#index'
   get 'pages/show'
   resources :questions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
